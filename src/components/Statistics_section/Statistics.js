4788ecd49be9f419118d2e385da9e78d6f2dcb53
Iryna Puchkova <irynapuchkova18@gmail.com>
2021-09-05 02:04:10 +0300

    add Statistics component

--- src/components/Statistics_section/Statistics.js
+++ src/components/Statistics_section/Statistics.js
@@ -1,25 +1,44 @@
 import PropTypes from 'prop-types';
+import s from './Statistics.module.css';
 
-
-
-export default function Statistics({ stats}) {
+export default function Statistics({ stats, title = 'Upload stats' }) {
   return (
-      <ul class="stat-list">
-        {stats.map(stat =>
-          <li class="item" key={stat.id}>
-            <span class="label">{stat.label}</span>
-            <span class="percentage">{stat.percentage}%</span>
+    <div className={s.stats}>
+      <h2 className={s.title}>{title}</h2>
+      <ul className={s.items}>
+        {stats.map(({ id, label, percentage }) => (
+          <li
+            className={`${s.item} ${s.label}`}
+            key={id}
+            style={{
+              backgroundColor:
+                label === '.pdf'
+                  ? 'red'
+                  : label === '.docs'
+                  ? 'blue'
+                  : label === '.mp3'
+                  ? 'green'
+                  : label === '.psd'
+                  ? 'yellow'
+                  : 'orange',
+            }}
+          >
+            <span className={s.label}>{label}</span>
+            <span className={s.percentage}>{percentage}%</span>
           </li>
-        )}
+        ))}
       </ul>
-    
-  )
+    </div>
+  );
 }
 
 Statistics.propTypes = {
-  stats: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    label: PropTypes.string.isRequired,
-    percentage: PropTypes.number.isRequired,
-  }))
-}
+  title: PropTypes.string,
+  stats: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      label: PropTypes.oneOf(['.docx', '.pdf', '.mp3', '.psd']).isRequired,
+      percentage: PropTypes.number.isRequired,
+    }),
+  ),
+};
